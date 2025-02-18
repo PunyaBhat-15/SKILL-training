456742d7719393db7522be910fc3f87eea41f852
PunyaBhat-15 <4MT23CI041@mite.ac.in>
2025-02-18 15:43:02 +0530

    Update day2_reveseString.c

--- day2_reveseString.c
+++ day2_reveseString.c
@@ -21,3 +21,39 @@ int main()
     printf("reversed string:%s\n",str);
     return 0;
 }
+2. Using the pointers:
+// str is the base address and p_ch holds it
+//de-referencing means coming bace to value memory
+#include <stdio.h>
+int findLenght(char* str)//using it as a charecter pointer while passing the array
+{
+    int length = 0;
+    for(char* p_ch = str; (*p_ch) !='\0'; p_ch++){
+        
+        length++;
+        
+    }
+    return length;
+}
+void reverseString(char *str)
+{
+    int length;
+    length=findLenght(str);
+    for(int left = 0, right = length-1; left<right; left++, right--){
+        char temp = *(str+ left);//use of pointer arthemetic same as str[left]
+        *(str+ left) = *(str+right);
+        *(str+right) = temp;
+    }
+    
+}
+
+int main()
+{
+    char str[255];
+    printf("Enter a string: ");
+    scanf("%s",str);
+    printf("before reverse:%s\n",str);
+    reverseString(str);
+    printf("reversed string:%s\n",str);
+    return 0;
+}
